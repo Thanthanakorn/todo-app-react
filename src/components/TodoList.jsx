cd2b991aaf2b4a93e2f4bb6db1b7f4442112c6b7
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2025-04-08 02:25:04 +0700

    display "add new task".

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,15 +3,20 @@ import {TodoCard} from "./TodoCard.jsx";
 export function TodoList(props) {
     const {todos} = props
 
-    const tab = 'All'
+    const tab = 'Open'
+    const filterTodosList = tab === 'All' ?
+        todos :
+        tab === 'Completed' ?
+            todos.filter(val => val.completed) :
+            todos.filter(val => !val.completed)
 
     return (
         <>
-            {todos.map((todo, todoIndex) => {
+            {filterTodosList.map((todo, todoIndex) => {
                 return(
                     <TodoCard key={todoIndex}
                               todoIndex={todoIndex}
-                              {...props}
+                              todo={todo}
                     />
                 )
             })}
